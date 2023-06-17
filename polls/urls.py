cbbe71e4949b666df91b76880f765269f6490945
likenoothersk1 <likenoothersk1@gamil.com>
2023-06-18 02:36:50 +0900

    업데이트1

--- polls/urls.py
+++ polls/urls.py
@@ -6,5 +6,4 @@ app_name = 'polls'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('king/<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
 ]
