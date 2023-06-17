54f60fa0e0e5e5becb30b4abcd7d6140e877fed3
likenoothersk1 <likenoothersk1@gamil.com>
2023-06-18 03:35:15 +0900

    일부러 conflict

--- polls/urls.py
+++ polls/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 from . import views
 app_name = 'polls'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('', views.IndexView.as_view(), name='index123'),
 ]
