a6ac3dd2d3fb6ccc030841db556f222f2a7092ff
likenoothersk1 <likenoothersk1@gamil.com>
2023-06-18 03:53:22 +0900

    변경

--- polls/urls.py
+++ polls/urls.py
@@ -4,5 +4,10 @@ from django.urls import path
 from . import views
 app_name = 'polls'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('', views.IndexView.as_view(), name='index'),        
+]
+
+urlpatterns = [
+    path('', views.IndexView, name='index'),
+    path('', views.IndexView, name='index123'),        
 ]
