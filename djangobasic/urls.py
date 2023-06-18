8039480ea6d1a930803a4a56fea3c5fda3e12097
likenoothersk1 <likenoothersk1@gamil.com>
2023-06-18 17:47:48 +0900

    Revert "urls.py 한줄삭제"
    This reverts commit be78f3e4583c3c683c4746b2fdfeabf0d4a68541.

--- djangobasic/urls.py
+++ djangobasic/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('tcamp/', include('tcamp.urls')),
     path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
 ]
