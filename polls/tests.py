a5e1d534179cb0f45925a619fd82116023cd29e0
likenoothersk1 <likenoothersk1@gamil.com>
2023-06-18 14:36:57 +0900

    delete

--- polls/tests.py
+++ polls/tests.py
@@ -10,15 +10,6 @@ from django.urls import reverse
 
 class QuestionModelTests(TestCase):
 
-    def test_was_published_recently_with_future_question(self):
-        """
-        was_published_recently() returns False for questions whose pub_date
-        is in the future.
-        """
-        time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(pub_date=time)
-        self.assertIs(future_question.was_published_recently(), False)
-
 def create_question(question_text, days):
     """
     Create a question with the given `question_text` and published the
