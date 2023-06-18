112c28b13fad8ded30785da2651028919f06d20e
likenoothersk1 <likenoothersk1@gamil.com>
2023-06-18 15:13:15 +0900

    Revert "?!"
    This reverts commit 1b928987983885752e4f33675b40f1f023f59a2d.

---
+++ polls/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path
+
+from . import views
+app_name = 'polls'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+]
+
+urlpatterns = [
+    path('', views.IndexView, name='index'),
+    path('', views.IndexView, name='index123'),        
+]
